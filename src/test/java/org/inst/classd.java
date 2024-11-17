f9812bba491360e8433b92a5065b76c805d49089
gke1404 <fresh.gke@gmail.com>
2024-11-17 15:19:08 +0530

    hi

---
+++ src/test/java/org/inst/classd.java
@@ -0,0 +1,14 @@
+package org.inst;
+
+public class classd {
+
+	
+	public void classd() {
+		System.out.println("123");
+	}
+	public static void main(String[] args) {
+		classd c = new classd();
+		
+		c.classd();
+	}
+}
