cab3b87fa828c090c922565ede0830ece8569a8a
gke1404 <fresh.gke@gmail.com>
2024-11-17 15:14:11 +0530

    new update

--- src/test/java/org/sd/SdMet.java
+++ src/test/java/org/sd/SdMet.java
@@ -4,12 +4,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
+
 public class SdMet {
 	
 	public static WebDriver driver;
 		
 	public void openBrow() {
-		
+			
 		WebDriver driver = new ChromeDriver();
 
 	}
@@ -19,8 +21,4 @@ public class SdMet {
 		driver.manage().window().maximize();
 
 	}
-	public void click(WebElement we) {
-		we.click();
-	}
-
 }
